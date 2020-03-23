60519b8cd2b8318843dd8a7daff5a541798051a8
TemurKhabibullaev <55756791+TemurKhabibullaev@users.noreply.github.com>
2020-03-23 18:49:42 -0400

    Done

--- Driver.py
+++ Driver.py
@@ -2,18 +2,18 @@
 from FIFO_LIFO_code import Queues as Q
 from FIFO_LIFO_code import Stacks as S
 
-# Creating two instances for amount and price Queues and Stacks
+# Creating two instances for amount Queues and Stacks
 instance_amountQ = Q()
-instance_priceQ = Q()
 
 instance_amountS = S()
-instance_priceS = S()
 
 
 def fifo():
     # We'll keep these two temporary variables (QUEUES)
     profit = 0
     inventory = 0
+    initial_price = None
+    one_time = 1
 
     while True:
         print("""You are in FIFO style. Choose below to proceed!
@@ -29,18 +29,20 @@ def fifo():
             amount = int(input("Please specify how many:\n>>>"))
             instance_amountQ.enqueue(int(amount))
             inventory += amount
-            price = int(input("Please specify hom much it cost:\n>>>"))
-            instance_priceQ.enqueue(int(price))
+            if one_time == 1:
+                price = int(input("Please specify hom much it costs:\n>>>"))
+                one_time -= 1
+                initial_price = price
         # Process of subtracting from inventory and adding to profits
         if select == 2:
             amount_to_sell = int(input("Specify how many will be deleted:\n>>>"))
+            stock_price = int(input("Hom much is it?:\n>>>"))
             # If specified amount is too much
             if amount_to_sell > inventory:
                 print("You specified too large amount.")
                 break
             else:
                 inventory -= amount_to_sell
-                last_price = None
                 # X is a first number in Queue
                 x = instance_amountQ.display()[0]
                 # When first number is less than amount to be deleted
@@ -51,26 +53,23 @@ def fifo():
                     while storage != amount_to_sell and storage < amount_to_sell:
                         selling_item = instance_amountQ.dequeue()
                         storage += selling_item
-                        last_price = instance_priceQ.dequeue()
                     # When enough is reached we will push back the difference and count the profit
                     if storage > amount_to_sell:
                         difference = storage - amount_to_sell
                         instance_amountQ.enqueue(difference)
-                        instance_priceQ.enqueue(last_price)
-                        profit += last_price * amount_to_sell
+                        profit += (stock_price * amount_to_sell) - (initial_price * amount_to_sell)
+                    if storage == amount_to_sell:
+                        profit += (stock_price * amount_to_sell) - (initial_price * amount_to_sell)
                 # When amount is less than first node in the Queue
                 if amount_to_sell < x and amount_to_sell > 0:
                     selling_item = instance_amountQ.dequeue()
-                    last_price = instance_priceQ.dequeue()
                     difference = selling_item - amount_to_sell
                     instance_amountQ.enqueue(difference)
-                    instance_priceQ.enqueue(last_price)
-                    profit += last_price * amount_to_sell
+                    profit += stock_price * amount_to_sell
                 # In case X equals to amount to sell
                 if x == amount_to_sell:
                     selling_item = instance_amountQ.dequeue()
-                    last_price = instance_priceQ.dequeue()
-                    profit += last_price * selling_item
+                    profit += stock_price * selling_item
         # This is to keep track of profits
         if select == 3:
             print("So far you have made:")
@@ -88,6 +87,9 @@ def lifo():
     # These are temporary variables (STACKS)
     profit = 0
     inventory = 0
+    initial_price = None
+    one_time = 1
+
     while True:
         print("""You are in LIFO style. Choose below to proceed!
         MENU:
@@ -102,11 +104,14 @@ def lifo():
             amount = int(input("Please specify how many:\n>>>"))
             instance_amountS.push(int(amount))
             inventory += amount
-            price = int(input("Please specify hom much it cost:\n>>>"))
-            instance_priceS.push(int(price))
+            if one_time == 1:
+                price = int(input("Please specify hom much it costs:\n>>>"))
+                one_time -= 1
+                initial_price = price
         # Process to sell items from the inventory
         if select == 2:
             amount_to_sell = int(input("Specify how many will be deleted:\n>>>"))
+            stock_price_ = int(input("How much is it?:\n>>>"))
             # If specified amount is too much
             if amount_to_sell > inventory:
                 print("You specified too large amount.")
@@ -115,8 +120,6 @@ def lifo():
             else:
                 # Subtracting selling amount from inventory
                 inventory -= amount_to_sell
-                # Having always changing variable for price
-                last_price = None
                 # X is the first number in the Stack
                 x = instance_amountS.display()[0]
                 # If first number is less than intended amount for sell
@@ -126,27 +129,24 @@ def lifo():
                     while storage != amount_to_sell and storage < amount_to_sell:
                         selling_item = instance_amountS.pop()
                         storage += selling_item
-                        last_price = instance_priceS.pop()
                     # When storage is surpassed do this
                     if storage > amount_to_sell:
                         difference = storage - amount_to_sell
                         instance_amountS.push(difference)
-                        instance_priceS.push(last_price)
-                        profit += last_price * amount_to_sell
+                        profit += (stock_price_ * amount_to_sell) - (initial_price * amount_to_sell)
+                    if storage == amount_to_sell:
+                        profit += (stock_price_ * amount_to_sell) - (initial_price * amount_to_sell)
                 # When amount for sell is less than first number in Stack
                 if amount_to_sell < x and amount_to_sell > 0:
                     # Process is easy
                     selling_item = instance_amountS.pop()
-                    last_price = instance_priceS.pop()
                     difference = selling_item - amount_to_sell
                     instance_amountS.push(difference)
-                    instance_priceS.push(last_price)
-                    profit += last_price * amount_to_sell
+                    profit += stock_price_ * amount_to_sell
                 # When it happens that first number equals to selling amount
                 if x == amount_to_sell:
                     selling_item = instance_amountS.pop()
-                    last_price = instance_priceS.pop()
-                    profit += last_price * selling_item
+                    profit += stock_price_ * selling_item
         # Display so far made profit
         if select == 3:
             print("So far you have made:")
